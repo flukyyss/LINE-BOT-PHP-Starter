398da507a349fb9b29fd4f9a09d8c5250a588ae6
Fluky <flukyysupp1@gmail.com>
2018-02-09 08:48:57 +0700

    bot

--- bot.php
+++ bot.php
@@ -28,7 +28,7 @@ if (!is_null($events['events'])) {
 						'type' => 'imagemap',
 						'baseUrl' => 'https://google.com'
 						'altText' => "This is an imagemap"
-						
+					];	
 			}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
