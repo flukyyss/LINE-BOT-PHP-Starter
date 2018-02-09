5ad04f484ae8b4441068ae2a3768be4b05bd1604
Fluky <flukyysupp1@gmail.com>
2018-02-09 08:25:25 +0700

    verification

--- bot.php
+++ bot.php
@@ -17,11 +17,12 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+			if($event['message']['text'] == 'hi'){
 			$messages = [
-				if($text == 'hi')
 				'type' => 'text',
 				'text' => $text
 			];
+			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
