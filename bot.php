fafaec42e033f5a85a14e07d28a2d93da50bc1d8
Fluky <flukyysupp1@gmail.com>
2018-02-09 10:51:19 +0700

    bot

--- bot.php
+++ bot.php
@@ -24,6 +24,8 @@ if (!is_null($events['events'])) {
 				'text' => 'สวัสดีครับผม'
 			];
 			}
+
+			
 		}
 		else if ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
 			
@@ -39,13 +41,27 @@ if (!is_null($events['events'])) {
 			];
 			
 		}
+			else if ($event['type'] == 'message' && $event['message']['type'] == 'image') {
+			
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+	
+			$messages = [
+				'type' => 'text',
+				'text' => 'กรุณารอการตอบรับสักครู่'
+
+			];
+			
+		}
 		
 			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages + 'hi'],
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
