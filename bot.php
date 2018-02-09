778c8b832a055e3419ce930559e5a7fa907d4b14
Fluky <flukyysupp1@gmail.com>
2018-02-09 10:41:15 +0700

    bot

--- bot.php
+++ bot.php
@@ -45,7 +45,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => [$messages + 'hi'],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
