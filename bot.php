a43675012a9c7b8410b2b564f3d35e75c3bd3e6d
Fluky <flukyysupp1@gmail.com>
2018-02-09 09:50:56 +0700

    verification

--- bot.php
+++ bot.php
@@ -35,9 +35,8 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 	
 			$messages = [
-				'type' => 'sticker',
-				'packageId' => '1'
-				'stickerId' => '1'
+				'type' => 'text',
+				'text' => 'sticker'
 			];
 			}
 		}
