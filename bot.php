8f6afc827d5e8ca0630c87ecbe9c318b1da7c87a
Fluky <flukyysupp1@gmail.com>
2018-02-09 08:38:19 +0700

    bot

--- bot.php
+++ bot.php
@@ -20,7 +20,7 @@ if (!is_null($events['events'])) {
 			if($event['message']['text'] == 'hi'){
 			$messages = [
 				'type' => 'text',
-				'text' => 'welcome'
+				'text' => 'สวัสดีครับ'
 			];
 			}
 
